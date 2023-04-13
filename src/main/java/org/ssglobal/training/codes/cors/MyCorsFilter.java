b79cf8cc10fab77509799f715ddd7fd54f13d7bb
patzluke <patzluke@gmail.comm>
2023-04-13 16:50:55 +0800

    8th commit, jwt token and headers now works

--- src/main/java/org/ssglobal/training/codes/cors/MyCorsFilter.java
+++ src/main/java/org/ssglobal/training/codes/cors/MyCorsFilter.java
@@ -5,15 +5,13 @@ import java.util.logging.Logger;
 
 import org.ssglobal.training.codes.repository.UserTokenRepository;
 
-import com.fasterxml.jackson.annotation.JacksonInject.Value;
-
 import jakarta.annotation.Priority;
 import jakarta.ws.rs.NotAuthorizedException;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
@@ -21,43 +19,40 @@ import jakarta.ws.rs.ext.Provider;
 
 @Provider
 @Priority(Priorities.AUTHENTICATION)
-public class MyCorsFilter implements ContainerResponseFilter {
+public class MyCorsFilter implements ContainerResponseFilter, ContainerRequestFilter {
 	private Logger logger = Logger.getLogger(MyCorsFilter.class.getName());
+	
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		UriInfo uriInfo = requestContext.getUriInfo();
+		String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+		if (!uriInfo.getPath().contains("users/get/query")) {			
+			if (authorizationHeader == null) {
+				throw new NotAuthorizedException("Authorization header must be provided");
+			}
+			String token = authorizationHeader.substring("Bearer".length()).trim();
 
-	@Context
+			if (!validateToken(token)) {
+				requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+			}
+		}
+	}
 	
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
 			throws IOException {
-		UriInfo uriInfo = requestContext.getUriInfo();
-		String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
-		if (!uriInfo.getPath().contains("users/get/query")) {
-			
-			logger.info(authorizationHeader + "pat header *********************");
-			if (authorizationHeader == null || !authorizationHeader.toString().startsWith("Bearer ")) {
-				throw new NotAuthorizedException("Authorization header must be provided");
-			}
-			try {
-				logger.info(authorizationHeader + " this is the token header pat");
-				validateToken(authorizationHeader);
-
-			} catch (Exception e) {
-				requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
-			}
-		}
-		
 		responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:8000");
 		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
 		responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
 		responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 	}
 
-	private void validateToken(String token) throws Exception {
+	private boolean validateToken(String token) {
 		UserTokenRepository userTokenRepository = new UserTokenRepository();
 		if (userTokenRepository.isUserTokenExists(token)) {
-			return;
-		} else {
-			throw new Exception("token not valid");
+			logger.info("nyeeeeeeeeeeeeeeee " + Boolean.valueOf(userTokenRepository.isUserTokenExists(token)).toString());
+			return true;
 		}
+		return false;
 	}
 }
